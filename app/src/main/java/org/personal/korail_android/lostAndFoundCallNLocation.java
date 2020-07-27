e64a83ce1a4e885f99763c8d36d9a950017d7de7
gayeon <leeedho67@gmail.com>
2020-07-27 13:59:13 +0900

    [add]유실물 센터 위치, 전화 액티비티, 레이아웃, 리소스 추가

---
+++ app/src/main/java/org/personal/korail_android/lostAndFoundCallNLocation.java
@@ -0,0 +1,14 @@
+package org.personal.korail_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lostAndFoundCallNLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lost_and_found_call_n_location);
+    }
+}
