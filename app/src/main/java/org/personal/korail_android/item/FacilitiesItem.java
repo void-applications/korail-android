7024ab01f45033bacc7460914f2ffab440c56b0b
99-zziy <wldud8229@gmail.com>
2020-07-27 15:54:09 +0900

    문화시설 아이템 추가

---
+++ app/src/main/java/org/personal/korail_android/item/FacilitiesItem.java
@@ -0,0 +1,68 @@
+package org.personal.korail_android.item;
+
+public class FacilitiesItem {
+
+    String id;
+    String lineName;
+    String stationName;
+    String floor;
+    String location;
+    String equipment;
+
+    public FacilitiesItem(String id, String lineName, String stationName, String floor, String location, String equipment) {
+        this.id = id;
+        this.lineName = lineName;
+        this.stationName = stationName;
+        this.floor = floor;
+        this.location = location;
+        this.equipment = equipment;
+    }
+
+    public String getEquipment() {
+        return equipment;
+    }
+
+    public void setEquipment(String equipment) {
+        this.equipment = equipment;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLineName() {
+        return lineName;
+    }
+
+    public void setLineName(String lineName) {
+        this.lineName = lineName;
+    }
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public void setStationName(String stationName) {
+        this.stationName = stationName;
+    }
+
+    public String getFloor() {
+        return floor;
+    }
+
+    public void setFloor(String floor) {
+        this.floor = floor;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
