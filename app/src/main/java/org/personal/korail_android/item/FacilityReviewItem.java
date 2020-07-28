dd91cf3e24e026e3126029b50eb34c81299c270d
99-zziy <wldud8229@gmail.com>
2020-07-28 13:10:52 +0900

    문화시설 리뷰 아이템 id값 추가

--- app/src/main/java/org/personal/korail_android/item/FacilityReviewItem.java
+++ app/src/main/java/org/personal/korail_android/item/FacilityReviewItem.java
@@ -1,14 +1,24 @@
 package org.personal.korail_android.item;
 
 public class FacilityReviewItem {
+    String id;
     String review;
     String date;
     int star;
 
-    public FacilityReviewItem(String review, String date, int star) {
+    public FacilityReviewItem(String id, String review, String date, int star) {
+        this.id = id;
         this.review = review;
         this.date = date;
         this.star = star;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getReview() {
