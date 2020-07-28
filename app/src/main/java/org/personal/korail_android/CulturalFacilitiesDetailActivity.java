1071b59fc52eba53eb6d887f55b3b815b581fa23
99-zziy <wldud8229@gmail.com>
2020-07-28 09:43:31 +0900

    문화시설 상세페이지에서 데이터 보여주기

--- app/src/main/java/org/personal/korail_android/CulturalFacilitiesDetailActivity.java
+++ app/src/main/java/org/personal/korail_android/CulturalFacilitiesDetailActivity.java
@@ -4,18 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.personal.korail_android.background.OkHttp;
+import org.personal.korail_android.item.FacilitiesItem;
 
 public class CulturalFacilitiesDetailActivity extends AppCompatActivity {
 
     String result;
+    Handler handler;
+    TextView stationNameTV, locationTV, equipmentTV, rentalCostTV, hoursOfUseTV, phoneNumberTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cultural_facilities_detail);
+
+        stationNameTV = findViewById(R.id.stationNameTV);
+        locationTV = findViewById(R.id.locationTV);
+        equipmentTV = findViewById(R.id.equipmentTV);
+        rentalCostTV = findViewById(R.id.rentalCostTV);
+        hoursOfUseTV = findViewById(R.id.hoursOfUseTV);
+        phoneNumberTV = findViewById(R.id.phoneNumberTV);
+
+        handler=new Handler();
 
         Intent intent = getIntent();
         final String id = intent.getStringExtra("id");
@@ -32,6 +50,37 @@ public class CulturalFacilitiesDetailActivity extends AppCompatActivity {
     }
 
     public void displayFacility(final String result) {
-        Log.e("result", result);
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                JSONObject jsonObject = null;
+                try {
+                    jsonObject = new JSONObject(result);
+                    //그중에서 data를 키값으로 갖는 jsonarray를 가져옴
+                    JSONArray jsonArray = jsonObject.getJSONArray("data");
+
+                        JSONObject searchItem = jsonArray.getJSONObject(0);
+                        String lineName = searchItem.getString("line_name");
+                        String stationName = searchItem.getString("station_name");
+                        String floor = searchItem.getString("floor");
+                        String location = searchItem.getString("location");
+                        String equipment = searchItem.getString("equipment");
+                        String phoneNumber=searchItem.getString("phone_number");
+                        String hoursOfUse=searchItem.getString("hours_of_use");
+                        String rentalCost=searchItem.getString("rental_cost");
+
+                        locationTV.setText(floor+" "+location);
+                        stationNameTV.setText(lineName+" "+stationName+"역");
+                        equipmentTV.setText(equipment);
+                        phoneNumberTV.setText(phoneNumber);
+                        hoursOfUseTV.setText(hoursOfUse);
+                        rentalCostTV.setText(rentalCost);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
