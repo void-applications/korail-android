a7e6e535349269d15a24d6e23447e48dcd987487
99-zziy <wldud8229@gmail.com>
2020-07-29 10:04:44 +0900

    리뷰 작성수정

--- app/src/main/java/org/personal/korail_android/CulturalFacilitiesDetailActivity.java
+++ app/src/main/java/org/personal/korail_android/CulturalFacilitiesDetailActivity.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -25,16 +26,15 @@ import java.util.ArrayList;
 
 public class CulturalFacilitiesDetailActivity extends AppCompatActivity {
 
-    String result,id;
+    String result, id;
     Handler handler;
-    RatingBar reviewRB;
-    EditText reviewET;
-    TextView stationNameTV, locationTV, equipmentTV, rentalCostTV, hoursOfUseTV, phoneNumberTV;
+    RatingBar reviewRB, averageRB;
+    TextView stationNameTV, locationTV, equipmentTV, rentalCostTV, hoursOfUseTV, phoneNumberTV, averageFloatTV;
+    ImageView locationIV, equipmentIV, rentalCostIV, hoursOfUseIV, phoneNumberIV;
     ArrayList<FacilityReviewItem> facilityReviewItemArrayList;
     FacilityReviewAdapter facilityReviewAdapter;
     LinearLayoutManager linearLayoutManager;
     RecyclerView recyclerView;
-    Button writeButton;
     OkHttp okHttpThread;
 
     @Override
@@ -49,9 +49,14 @@ public class CulturalFacilitiesDetailActivity extends AppCompatActivity {
         hoursOfUseTV = findViewById(R.id.hoursOfUseTV);
         phoneNumberTV = findViewById(R.id.phoneNumberTV);
         recyclerView = findViewById(R.id.facilitiesReviewRecyclerview);
-        writeButton = findViewById(R.id.writeButton);
-        reviewET=findViewById(R.id.reviewET);
-        reviewRB=findViewById(R.id.reviewRB);
+        reviewRB = findViewById(R.id.reviewRB);
+        averageRB = findViewById(R.id.averageStarRB);
+        phoneNumberIV = findViewById(R.id.phoneNumberIV);
+        locationIV = findViewById(R.id.locationIV);
+        rentalCostIV = findViewById(R.id.rentalCostIV);
+        equipmentIV = findViewById(R.id.equipmentIV);
+        hoursOfUseIV = findViewById(R.id.hoursOfUseIV);
+        averageFloatTV = findViewById(R.id.averageFloatTV);
 
         facilityReviewItemArrayList = new ArrayList<>();
         facilityReviewAdapter = new FacilityReviewAdapter(facilityReviewItemArrayList, this);
@@ -61,6 +66,7 @@ public class CulturalFacilitiesDetailActivity extends AppCompatActivity {
 
         handler = new Handler();
         okHttpThread = new OkHttp();
+        reviewRB.setOnRatingBarChangeListener(new Listener());
 
         Intent intent = getIntent();
         id = intent.getStringExtra("id");
@@ -73,29 +79,14 @@ public class CulturalFacilitiesDetailActivity extends AppCompatActivity {
                 displayFacility(result);
             }
         }).start();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
 
         getReview();
-
-
-        writeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                JSONObject jsonObject=new JSONObject();
-                try {
-                    jsonObject.accumulate("id",id);
-                    jsonObject.accumulate("review",reviewET.getText());
-                    jsonObject.accumulate("star",reviewRB.getNumStars());
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                final String json=jsonObject.toString();
-
-                postReview(json);
-                reviewET.setText("");
-            }
-        });
-
-
+        reviewRB.setRating(0);
     }
 
     public void displayFacility(final String result) {
@@ -118,33 +109,31 @@ public class CulturalFacilitiesDetailActivity extends AppCompatActivity {
                     String hoursOfUse = searchItem.getString("hours_of_use");
                     String rentalCost = searchItem.getString("rental_cost");
 
-                    locationTV.setText("위치 : "+floor + " " + location);
+                    locationTV.setText("위치 : " + floor + " " + location);
                     stationNameTV.setText(lineName + " " + stationName + "역");
-                    if(!equipment.equals("null")){
-                        equipmentTV.setText("장비 : "+equipment);
-                    }
-                    else{
+                    if (!equipment.equals("null")) {
+                        equipmentTV.setText("장비 : " + equipment);
+                    } else {
                         equipmentTV.setVisibility(View.GONE);
+                        equipmentIV.setVisibility(View.GONE);
                     }
-                    if(!phoneNumber.equals("null")){
-                        phoneNumberTV.setText("전화번호 : "+phoneNumber);
-                    }
-                    else{
+                    if (!phoneNumber.equals("null")) {
+                        phoneNumberTV.setText("전화번호 : " + phoneNumber);
+                    } else {
                         phoneNumberTV.setVisibility(View.GONE);
-
-
+                        phoneNumberIV.setVisibility(View.GONE);
                     }
-                    if(!hoursOfUse.equals("null")) {
+                    if (!hoursOfUse.equals("null")) {
                         hoursOfUseTV.setText("이용시간 : " + hoursOfUse);
-                    }
-                    else{
+                    } else {
                         hoursOfUseTV.setVisibility(View.GONE);
+                        hoursOfUseIV.setVisibility(View.GONE);
                     }
-                    if(!rentalCost.equals("null")){
-                        rentalCostTV.setText("대여비용 : "+rentalCost+" 원");
-                    }
-                    else{
+                    if (!rentalCost.equals("null")) {
+                        rentalCostTV.setText("대여비용 : " + rentalCost + " 원");
+                    } else {
                         rentalCostTV.setVisibility(View.GONE);
+                        rentalCostIV.setVisibility(View.GONE);
                     }
 
 
@@ -160,6 +149,7 @@ public class CulturalFacilitiesDetailActivity extends AppCompatActivity {
             @Override
             public void run() {
                 JSONObject jsonObject = null;
+                ArrayList<Integer> starList = new ArrayList();
                 facilityReviewItemArrayList.clear();
                 facilityReviewAdapter.notifyDataSetChanged();
                 try {
@@ -177,8 +167,17 @@ public class CulturalFacilitiesDetailActivity extends AppCompatActivity {
                         FacilityReviewItem facilityReviewItem = new FacilityReviewItem(id, review, date, Integer.parseInt(star));
                         facilityReviewItemArrayList.add(facilityReviewItem);
                         facilityReviewAdapter.notifyDataSetChanged();
+
+                        starList.add(Integer.parseInt(star));
                     }
 
+                    int sum = 0;
+                    for (int i = 0; i < starList.size(); i++) {
+                        sum += starList.get(i);
+                    }
+                    float average = sum / (float) starList.size();
+                    averageFloatTV.setText(average+" 점");
+                    averageRB.setRating(average);
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -187,7 +186,7 @@ public class CulturalFacilitiesDetailActivity extends AppCompatActivity {
         });
     }
 
-    public void getReview(){
+    public void getReview() {
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -198,15 +197,16 @@ public class CulturalFacilitiesDetailActivity extends AppCompatActivity {
         }).start();
     }
 
-    public void postReview(final String json){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
 
-                okHttpThread.postData("http://52.79.146.35/facility_review",json);
-                displayFacilityReview(result);
-                getReview();
-            }
-        }).start();
+    class Listener implements RatingBar.OnRatingBarChangeListener {
+
+        @Override
+        public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+            int starNum = (int) reviewRB.getRating();
+            Intent ratingIntent = new Intent(getApplicationContext(), WriteReviewActivity.class);
+            ratingIntent.putExtra("id", id);
+            ratingIntent.putExtra("starNum", starNum);
+            startActivity(ratingIntent);
+        }
     }
 }
