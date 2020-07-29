8aa636be2de031ba903dd558233a9582279e7608
ByungHakNoh <nbh6579@naver.com>
2020-07-29 18:41:02 +0900

    [modify] 메인 액티비티 : 홈으로 이동하도록 변경

--- app/src/main/java/org/personal/korail_android/MainActivity.kt
+++ app/src/main/java/org/personal/korail_android/MainActivity.kt
@@ -1,5 +1,6 @@
 package org.personal.korail_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
